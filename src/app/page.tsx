aed6365935853295a7bffddb4ef263257c8d20d0
Jude Hannu <jude@judehannu.dev>
2024-04-23 15:45:44 -0700

    Changed layout, added basic vercel postgres

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,42 @@
 import Link from "next/link";
+import { db } from "~/server/db";
 
-export default function HomePage() {
+const mockUrls = [
+  "https://utfs.io/f/4c70b9b1-5c91-46ad-b7d9-0267f475d22f-2u6cjz.jpg",
+  "https://utfs.io/f/b0adac19-ea05-45cb-8734-2b27cec041fd-we8qu5.jpg",
+  "https://utfs.io/f/e4dd4f53-1672-4ee7-8a8b-14da9306ec31-29xeb9.jpg"
+]
+
+const mockImages = mockUrls.map((url, index) => ({
+  id: index + 1,
+  url,
+}))
+
+// The above code returns:
+// [
+//   { id: 1, url: 'https://utfs.io/f/4c70b9b1-5c91-46ad-b7d9-0267f475d22f-2u6cjz.jpg' },
+//   { id: 2, url: 'https://utfs.io/f/b0adac19-ea05-45cb-8734-2b27cec041fd-we8qu5.jpg' },
+//   { id: 3, url: 'https://utfs.io/f/e4dd4f53-1672-4ee7-8a8b-14da9306ec31-29xeb9.jpg' }
+// ]
+
+export default async function HomePage() {
+
+  const posts = await db.query.posts.findMany();
+
+  console.log(posts);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      Hi (construction in progress here)
+    <main className="">
+      <div className="flex flex-wrap gap-4">
+        {posts.map((post) => (
+          <div key={post.id}>{post.name}</div>
+        ))}
+        {[...mockImages, ...mockImages, ...mockImages].map((image, index) => (
+          <div key={image.id + index} className="w-48">
+            <img src={image.url} alt="image" />
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
